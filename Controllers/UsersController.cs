506d2fa7137a08fac92c16d6d1cedcfcedd73e50
John Mansfield <jmansfield6@gatech.edu>
2017-08-16 19:43:32 -0400

    added readme.md and connection template for MS SQL Server

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+//parts taken from 
+//https://stackoverflow.com/questions/6694098/using-asp-net-mvc-with-classic-ado-net
+
+namespace WebApplication5.Controllers
+{
+  
+  public class UsersController : Controller
+    {
+    
+    private readonly IUsersRepository _repository;
+    public UsersController(IUsersRepository repository)
+    {
+      _repository = repository;
+    }
+
+    public ActionResult Index(int id)
+    {
+      var model = _repository.GetTableData();
+      return View(model);
+    }
+  }
+}
---
+++ Models/UsersRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Linq;
+
+namespace WebApplication5.Models
+{
+  public class UsersRepository : IUsersRepository
+  {
+    private readonly string _connectionString;
+    public UsersRepository(string connectionString)
+    {
+      _connectionString = connectionString;
+    }
+
+    public String GetTableData()
+    {
+
+      using (var conn = new SqlConnection(_connectionString))
+      using (var cmd = conn.CreateCommand())
+      {
+        conn.Open();
+        cmd.CommandText = "Select * from dbo.Table_1";
+        conn.Close();
+        //cmd.Parameters.AddWithValue("@id", id);
+       }
+      return "";
+      }
+    }
+  }
