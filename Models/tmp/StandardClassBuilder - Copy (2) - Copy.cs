0765c6b8d8332a7b8fa157217282c50dddf9e378
John Mansfield <jmansfield6@gatech.edu>
2017-08-16 16:23:24 -0400

    8162017 - 5

---
+++ Models/tmp/StandardClassBuilder - Copy (2) - Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//StandardClassBuilder implements ClassBuilder interface
+public class StandardClassBuilder: ClassBuilder
+{
+  public StandardClassBuilder()
+  {
+    
+  }
+
+  public virtual void buildA() { }
+  public virtual void buildB() { }
+
+  public virtual void buildC() { }
+
+
+}
